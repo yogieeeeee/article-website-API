1acd3a38925e16f554f3999ea52922deacc03173
yogieeeeee <yogieaditiya976@gmail.com>
2025-04-22 00:45:35 +0700

    add auth system with oAuth JWT

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import express from "express"
 import upload from "express-fileupload"
 import connectDB from "./config/db.js"
 import createAdmin from "./setup/createAdmin.js"
+import authRoute from "./routers/auth.route.js"
 const app = express()
 
 connectDB()
 createAdmin()
 app.use(express.json())
 app.use(upload())
+
+//authorization route
+app.use("/api/auth", authRoute)
 
 app.listen(5000, () => {
   console.log("server is running on port 5000")
